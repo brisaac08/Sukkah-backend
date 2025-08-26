f93db2eb40f0f43620dd4fa6ac0314a387a921ff
Francisco Quintero <111946228+Francisco-Quintero@users.noreply.github.com>
2025-08-25 23:46:12 -0500

    Backend

---
+++ src/main/java/com/example/Sukkah/SukkahApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sukkah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukkahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukkahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sukkah/SukkahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sukkah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukkahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
